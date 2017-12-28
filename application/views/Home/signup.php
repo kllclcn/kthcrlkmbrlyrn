e46f3dce7435765b86bc4f28225e05491cb792f4
kllclcn <kimcalucin@gmail.com>
2017-12-28 19:00:30 +0800

    signup.php

--- application/views/Home/signup.php
+++ application/views/Home/signup.php
@@ -12,6 +12,9 @@ background-color: #dc4343;
 
 <head>
 	<title> PUP Buy and Sell </title>
+	<link rel="icon" href="<?php echo base_url(); ?>images/pupseal.png">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/designsignup.css">
+	<img src="<?php echo base_url(); ?>images/main.png" alt="Background" style='width: 100%;'/>
 	
 </head>
 
@@ -22,14 +25,10 @@ background-color: #dc4343;
   		<li><a href="home.html">HOME</a></li>
   		<li><a href="about.html" >ABOUT</a></li>
   		<li style="color: #000; background-color: #f8f9f9;">SIGN UP</li>
-  		<li><a href="login.html" >LOG IN</a></li>
+  		<li><a href="login" >LOG IN</a></li>
 	</ul>
 	</div>
-	
-
-	<div >
-		<h2 id="gs">GETTING STARTED</h2>
-	</div>
+	 
 
 	<ul class="ul2">
   		<li>STEP 1</li>
@@ -38,7 +37,48 @@ background-color: #dc4343;
   		<li>STEP 4</li>
 	</ul>
 
+	<div id="steps" >
+	
+		<form id="step1">
+			<div class="container">
+				<label id="sulbl"><b>Create Username:</b></label>
+				<input type="text" placeholder="Username" name="username" required><br/><br/>
 
+				<label id="sulbl"><b>Email Address:</b></label>
+				<input type="email" placeholder="Email Address" name="email" required><br/><br/>
 
+				<label id="sulbl"><b>Password:</b></label>
+				<input type="password" placeholder="Password" name="password" required><br/><br/>
+
+				<label id="sulbl"><b> Confirm Password:</b></label>
+				<input type="password" placeholder="Confirm Password" name="password" required><br/><br/>
+			
+			</div>
+		</form>
+
+		<form id="step1">
+			<div class="container">
+				<label id="sulbl"><b>Full Name:</b></label>
+				<input type="text" placeholder="Full Name" name="fullname" required><br/><br/>
+
+				<label id="sulbl"><b>Address:</b></label>
+				<input type="text" placeholder="Address" name="address" required><br/><br/>
+
+				<label id="sulbl"><b>Age:</b></label><br/>
+				<input type="radio" name="age" value="male" required> Male<br/>
+				<input type="radio" name="age" value="female" required> Female<br/><br/>
+
+				<label id="sulbl"><b>Birthday:</b></label>
+				<input type="date" name="bdate" required><br/><br/>
+
+				<label id="sulbl"><b>Mobile Number:</b></label>
+				<input type="number" placeholder="Mobile Number" name="mobnum" required><br/><br/>
+
+				<input type="submit" name="prev1" value="PREVIOUS"><br/><br/>
+				<input type="submit" name="next2" value="NEXT"><br/><br/><br/>
+			</div>
+		</form>				
+
+	</div>
 </body>
 </html>
