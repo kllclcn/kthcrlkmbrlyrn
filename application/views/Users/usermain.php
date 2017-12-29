948f1a6bb87f2bca0f47fc0dad8c66cc84e4f0ba
kllclcn <kimcalucin@gmail.com>
2017-12-29 22:49:52 +0800

    test

--- application/views/Users/usermain.php
+++ application/views/Users/usermain.php
@@ -1,8 +1,31 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html lang="en">
 
+<style>
+body{
+background-color: #dc4343;
+}
+</style>
+
+<head>
+	<title>LOGIN / PUP BUY AND SELL </title>
+	<link rel="icon" href="<?php echo base_url(); ?>images/pupseal.png">
+                      <link rel="stylesheet" href="<?php echo base_url(); ?>css/designusermain.css">
+	<img src="<?php echo base_url(); ?>images/mains.png" alt="Background" style='width: 100%;'/>
+	
+	
+</head>
+
+<body>
+    <div class="layy">
+        <h> Wecome (NameofUser)! </h>
+    </div>
+    <div class="badi">
+        <a href="sell">SELL</a>
+        <a href="buy">BUY</a>
+    </div>
+</body>
+</html>
