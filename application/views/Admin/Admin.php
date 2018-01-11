29156bf6f25e9186bb270bc70fff292438cf0780
kllclcn <kimcalucin@gmail.com>
2018-01-11 14:05:47 +0800

    Merge branch 'kim' of https://github.com/kllclcn/kthcrlkmbrlyrn into carl

--- application/views/Admin/Admin.php
+++ application/views/Admin/Admin.php
@@ -2,10 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
+        <style>
+body{
+background-color: #dc4343;
+}
+</style>
 <head>
 	<title>ADMIN</title>
 	<link rel="icon" href="<?php echo base_url(); ?>images/pupseal.png">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>css/designadminadmin.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/designadminadmins.css">
         <img src="<?php echo base_url(); ?>images/mains.png" alt="Background" style='width: 100%;'/>
 </head>
 <body>
