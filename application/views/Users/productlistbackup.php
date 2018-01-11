1177138a743526e7c294c930d025c6b5ee0b833f
kllclcn <kimcalucin@gmail.com>
2018-01-11 14:55:39 +0800

    Merge branch 'kim' of https://github.com/kllclcn/kthcrlkmbrlyrn into carl

--- application/views/Users/productlistbackup.php
+++
@@ -1,9 +0,0 @@
-<tr>
-	<td><?php echo $prod_name; ?></td>
-	<td><?php echo $price; ?></td>
-	<td><?php echo $place; ?></td>
-	
-	<td>
-		<a href='Btrans/?id=<?php echo $prod_id; ?>'>Buy</a> 
-	</td>
-</tr>
